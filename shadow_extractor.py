f65b707505cfe19c1c30d3fea05de074346a3ef7
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2024-06-14 16:33:54 -0700

    Update shadow_extractor.py

--- shadow_extractor.py
+++ shadow_extractor.py
@@ -1,10 +1,35 @@
 # --- Shadow Extractor ---
-# This script retrieves encrypted passwords from the /etc/shadow file on a Linux system.
+# This script retrieves encrypted passwords from the /etc/shadow file on a Linux system, and identifies the hashing algorithm used.
 # Note: Elevated privileges are required to run the script, i.e. 'sudo python3 shadow_extractor.py'
 
 import os
 path = '/etc'
 os.chdir(path)
+
+
+def hash_type(input):
+    alg = ''
+    count = 0
+    for char in input:
+        if count < 2:
+            if char == '$':
+                count += 1
+            alg += char
+    if alg == '$1$':
+        print('MD5 algorithm detected:', alg)
+    elif alg == '$2a$':
+        print('Blowfish algorithm detected:', alg)
+    elif alg == '$2y$':
+        print('Blowfish algorithm detected:', alg)
+    elif alg == '$5$':
+        print('SHA-256 algorithm detected:', alg)
+    elif alg == '$6$':
+        print('SHA-512 algorithm detected:', alg)
+    elif alg == '$y$':
+        print('Yescrypt algorithm detected:', alg)
+    else:
+        print('Unknown algorithm detected.')
+
 
 def shadow_parse(target_file):
     for line in target_file:
@@ -18,6 +43,8 @@ def shadow_parse(target_file):
         # This line excludes users with no password retrievable.
         if not password.startswith('!') and not password.startswith('*'):
             print(uname + ' ' + password)
+            hash_type(password)
+
 
 read_shadow = open('shadow', 'r')
 lines = read_shadow.readlines()
